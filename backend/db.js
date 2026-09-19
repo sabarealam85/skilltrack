3f499d53bbce09b51fedf20f9f81560659e2a045
SABARE ALAM <sabarealam@85gmail.com>
2026-09-20 01:49:22 +0530

    Fix Neon database connection

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    },
-    options: "-c search_path=public"
+    }
 });
 
 module.exports = pool;
