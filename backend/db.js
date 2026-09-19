6ff06a5a4f19f739190cdec02f0525969a6a0ab1
SABARE ALAM <sabarealam@85gmail.com>
2026-09-20 01:42:44 +0530

    Fix PostgreSQL schema search path

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+    options: "-c search_path=public"
 });
 
 module.exports = pool;
